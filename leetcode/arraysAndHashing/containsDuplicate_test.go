b36b96aab02be918a7899b00ed3ef8079a9f7139
vchiranjeeviak <vchiranjeeviak.tirunagari@gmail.com>
2023-10-15 22:34:54 +0530

    chore: fmt

--- leetcode/arraysAndHashing/containsDuplicate_test.go
+++ leetcode/arraysAndHashing/containsDuplicate_test.go
@@ -3,48 +3,48 @@ package arraysandhashing
 import "testing"
 
 type containsDuplicateTestCase struct {
-    array []int
-    result bool
+	array  []int
+	result bool
 }
 
 func containsDuplicateGetTestCase(num int) containsDuplicateTestCase {
 	switch num {
 	case 1:
 		return containsDuplicateTestCase{
-            []int{1, 2, 3, 1},
-            true,
-        }
+			[]int{1, 2, 3, 1},
+			true,
+		}
 	case 2:
 		return containsDuplicateTestCase{
-            []int{1, 2, 3, 4},
-            false,
-        }
+			[]int{1, 2, 3, 4},
+			false,
+		}
 	case 3:
 		return containsDuplicateTestCase{
-            []int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2},
-            true,
-        }
+			[]int{1, 1, 1, 3, 3, 4, 3, 2, 4, 2},
+			true,
+		}
 	}
 	return containsDuplicateTestCase{
-        []int{1},
-        false,
-    }
+		[]int{1},
+		false,
+	}
 }
 
 func TestContainsDuplicate1(t *testing.T) {
-    testCase1 := containsDuplicateGetTestCase(1)
+	testCase1 := containsDuplicateGetTestCase(1)
 	result := containsDuplicate1(testCase1.array)
 	if result != testCase1.result {
 		t.Error("Test case 1 failed for approach 1.")
 	}
 
-    testCase2 := containsDuplicateGetTestCase(2)
+	testCase2 := containsDuplicateGetTestCase(2)
 	result = containsDuplicate1(testCase2.array)
 	if result != testCase2.result {
 		t.Error("Test case 2 failed for approach 1.")
 	}
 
-    testCase3 := containsDuplicateGetTestCase(3)
+	testCase3 := containsDuplicateGetTestCase(3)
 	result = containsDuplicate1(testCase3.array)
 	if result != testCase3.result {
 		t.Error("Test case 3 failed for approach 1.")
@@ -52,19 +52,19 @@ func TestContainsDuplicate1(t *testing.T) {
 }
 
 func TestContainsDuplicate2(t *testing.T) {
-    testCase1 := containsDuplicateGetTestCase(1)
+	testCase1 := containsDuplicateGetTestCase(1)
 	result := containsDuplicate2(testCase1.array)
 	if result != testCase1.result {
 		t.Error("Test case 1 failed for approach 2.")
 	}
 
-    testCase2 := containsDuplicateGetTestCase(2)
+	testCase2 := containsDuplicateGetTestCase(2)
 	result = containsDuplicate2(testCase2.array)
 	if result != testCase2.result {
 		t.Error("Test case 2 failed for approach 2.")
 	}
 
-    testCase3 := containsDuplicateGetTestCase(3)
+	testCase3 := containsDuplicateGetTestCase(3)
 	result = containsDuplicate2(testCase3.array)
 	if result != testCase3.result {
 		t.Error("Test case 3 failed for approach 2.")
@@ -72,19 +72,19 @@ func TestContainsDuplicate2(t *testing.T) {
 }
 
 func TestContainsDuplicate3(t *testing.T) {
-    testCase1 := containsDuplicateGetTestCase(1)
+	testCase1 := containsDuplicateGetTestCase(1)
 	result := containsDuplicate3(testCase1.array)
 	if result != testCase1.result {
 		t.Error("Test case 1 failed for approach 3.")
 	}
 
-    testCase2 := containsDuplicateGetTestCase(2)
+	testCase2 := containsDuplicateGetTestCase(2)
 	result = containsDuplicate3(testCase2.array)
 	if result != testCase2.result {
 		t.Error("Test case 2 failed for approach 3.")
 	}
 
-    testCase3 := containsDuplicateGetTestCase(3)
+	testCase3 := containsDuplicateGetTestCase(3)
 	result = containsDuplicate3(testCase3.array)
 	if result != testCase3.result {
 		t.Error("Test case 3 failed for approach 3.")
--- leetcode/arraysAndHashing/validAnagram_test.go
+++ leetcode/arraysAndHashing/validAnagram_test.go
@@ -3,31 +3,31 @@ package arraysandhashing
 import "testing"
 
 type validAnagramTestCase struct {
-    array []string
-    result bool
+	array  []string
+	result bool
 }
 
 func validAnagramGetTestCase(num int) validAnagramTestCase {
 	switch num {
 	case 1:
 		return validAnagramTestCase{
-            []string{"anagram", "nagaram"},
-            true,
-        }
+			[]string{"anagram", "nagaram"},
+			true,
+		}
 	case 2:
 		return validAnagramTestCase{
-            []string{"rat", "car"},
-            false,
-        }
+			[]string{"rat", "car"},
+			false,
+		}
 	}
 	return validAnagramTestCase{
-        []string{"", ""},
-        true,
-    }
+		[]string{"", ""},
+		true,
+	}
 }
 
 func TestValidAnagram1(t *testing.T) {
-    testCase1 := validAnagramGetTestCase(1)
+	testCase1 := validAnagramGetTestCase(1)
 	stringS := testCase1.array[0]
 	stringT := testCase1.array[1]
 	isValidAnagram := isAnagram1(stringS, stringT)
@@ -36,7 +36,7 @@ func TestValidAnagram1(t *testing.T) {
 		t.Error("Test case 1 failed for approach 1.")
 	}
 
-    testCase2 := validAnagramGetTestCase(2)
+	testCase2 := validAnagramGetTestCase(2)
 	stringS = testCase2.array[0]
 	stringT = testCase2.array[1]
 	isValidAnagram = isAnagram1(stringS, stringT)
@@ -47,7 +47,7 @@ func TestValidAnagram1(t *testing.T) {
 }
 
 func TestValidAnagram2(t *testing.T) {
-    testCase1 := validAnagramGetTestCase(1)
+	testCase1 := validAnagramGetTestCase(1)
 	stringS := testCase1.array[0]
 	stringT := testCase1.array[1]
 	isValidAnagram := isAnagram2(stringS, stringT)
@@ -56,7 +56,7 @@ func TestValidAnagram2(t *testing.T) {
 		t.Error("Test case 1 failed for approach 2.")
 	}
 
-    testCase2 := validAnagramGetTestCase(2)
+	testCase2 := validAnagramGetTestCase(2)
 	stringS = testCase2.array[0]
 	stringT = testCase2.array[1]
 	isValidAnagram = isAnagram2(stringS, stringT)
